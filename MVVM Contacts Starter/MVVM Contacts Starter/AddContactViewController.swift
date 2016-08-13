707e87dcedf02b32952180d9043484ff6e5fafe5
Rafael Sacchi <rafaelsacchi@Rafaels-MacBook-Pro.local>
2016-08-13 19:17:29 -0300

    Starter project set up

---
+++ MVVM Contacts Starter/MVVM Contacts Starter/AddContactViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddContactViewController.swift
+//  MVVM Contacts Starter
+//
+//  Created by Rafael Sacchi on 8/13/16.
+//  Copyright © 2016 Rafael Sacchi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ MVVM Contacts Starter/MVVM Contacts Starter/ContactsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MVVM Contacts Starter
+//
+//  Created by Rafael Sacchi on 8/13/16.
+//  Copyright © 2016 Rafael Sacchi. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
